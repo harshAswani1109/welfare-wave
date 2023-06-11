7b73977c839d34de3bcd8a02b97b0dd4d3f1e7ee
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-06-11 11:49:51 +0530

    first commit

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import MobileNavbar from "./mobileNavbar";
+import Link from "next/link";
 
 const Navbar = () => {
   const [dropdownOpen, setdropdownOpen] = useState(false);
@@ -10,8 +11,36 @@ const Navbar = () => {
   };
   return (
     <nav>
-      <div className="flex justify-between items-center px-10 h-20">
+      <div className="flex justify-between items-center px-10 h-20 ">
         <div>HI I'am Navbar</div>
+        <div className="hidden md:block">
+          <div className="ml-10 flex items-baseline space-x-4">
+            <Link
+              href="#"
+              className="text-gray-00 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-00 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              About
+            </Link>
+            <Link
+              href="/information  "
+              className="text-gray-00 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Information
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-00 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
         <div className="md:hidden">
           <RxHamburgerMenu onClick={handleFunction} />
         </div>
