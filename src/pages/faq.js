4940440dedcc4cf80ea136e64e07638286f7b6a2
sashikanta mohanty <sm8168@srmist.edu.in>
2023-06-12 03:22:04 +0530

    FAQ's added

---
+++ src/pages/faq.js
@@ -0,0 +1,32 @@
+import { Inter } from 'next/font/google'
+import { useState } from 'react'
+import Layout from '../../components/layout.js'
+import data from "../../public/data.json"
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function Home() {
+    const [active, setActive] = useState([false, false, false, false, false]);
+    const isSomeActive = active.some((element) => element);
+    const handleClick = () => {
+        isSomeActive
+        ? setActive([false, false, false, false,false])
+        : setActive([true, true, true, true, true, true]);
+    };
+
+    return (
+    <>
+        
+        <div className='grid place-items-center w-full'>
+        <Layout
+            handleClick={handleClick}
+            isSomeActive={isSomeActive}
+            data={data}
+            turn={active}
+            setTurn={setActive}
+        />
+        </div>
+
+        </>
+    )
+}
