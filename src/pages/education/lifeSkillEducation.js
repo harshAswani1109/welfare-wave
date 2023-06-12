fdd867a4ee8be673ff86aafca504d621c7288919
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-06-12 08:33:31 +0530

    added life skill training

--- src/pages/education/lifeSkillEducation.js
+++ src/pages/education/lifeSkillEducation.js
@@ -1,28 +1,76 @@
-const Information = () => {
+const LifeSkillsTraining = () => {
   const data = [
     {
-      userId: 1,
       id: 1,
-      title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipitsuscipit recusandae consequuntur expedita et cumreprehenderit molestiae ut ut quas totamnostrum rerum est autem sunt rem eveniet architecto",
+      title: "Effective Communication",
+      list1:
+        "Understanding the importance of active listening and expressing oneself clearly.",
+      list2: "Developing effective verbal and non-verbal communication skills.",
+      list3: "Practicing empathy and understanding others' perspectives.",
+      list4:
+        "Enhancing communication in personal and professional relationships.",
     },
     {
-      userId: 1,
       id: 2,
-      title: "qui est esse",
-      body: "est rerum tempore vitaesequi sint nihil reprehenderit dolor beatae ea dolores nequefugiat blanditiis voluptate porro vel nihil molestiae ut reiciendisqui aperiam non debitis possimus qui neque nisi nulla",
+      title: "Time Management",
+      list1: "Setting clear goals and priorities.",
+      list2:
+        "Developing strategies to prioritize tasks and manage time effectively.",
+      list3: "Overcoming procrastination and staying focused.",
+      list4: "Creating schedules and routines for better time management.",
     },
     {
-      userId: 1,
       id: 3,
-      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      body: "et iusto sed quo iurevoluptatem occaecati omnis eligendi aut advoluptatem doloribus vel accusantium quis pariaturmolestiae porro eius odio et labore et velit aut",
+      title: "Problem-Solving and Decision-Making",
+      list1: "Analyzing problems and identifying possible solutions.",
+      list2: "Evaluating pros and cons and making informed decisions.",
+      list3:
+        "Applying critical thinking and creative problem-solving techniques.",
+      list4: "Building resilience and adapting to unexpected challenges.",
+    },
+    {
+      id: 4,
+      title: "Emotional Intelligence",
+      list1: "Understanding and managing emotions effectively.",
+      list2: "Developing self-awareness and self-regulation.",
+      list3: "Empathy and understanding others' emotions.",
+      list4:
+        "Building positive relationships and resolving conflicts constructively.",
+    },
+    {
+      id: 5,
+      title: "Stress Management",
+      list1: "Recognizing signs of stress and its impact on well-being.",
+      list2:
+        "Practicing relaxation techniques, such as deep breathing and mindfulness.",
+      list3: "Developing healthy coping mechanisms and self-care strategies.",
+      list4: "Balancing work, personal life, and leisure activities.",
+    },
+    {
+      id: 6,
+      title: "Financial Literacy",
+      list1:
+        "Understanding basic financial concepts, such as budgeting, saving, and investing.",
+      list2: "Managing personal finances effectively and avoiding debt.",
+      list3:
+        "Making informed decisions regarding expenses and financial planning.",
+      list4: "Building a strong foundation for long-term financial well-being.",
+    },
+    {
+      id: 7,
+      title: "Adaptability and Resilience",
+      list1: "Embracing change and managing transitions effectively.",
+      list2: "Developing flexibility and adaptability in various situations.",
+      list3: "Building resilience to bounce back from setbacks and challenges.",
+      list4: "Cultivating a growth mindset and embracing continuous learning.",
     },
   ];
+
   return (
     <div>
-      <h1 className="font-bold">Information</h1>
+      <h1 className="font-bold text-center w-full text-3xl tracking-wide">
+        Life Skills Training
+      </h1>
       <div className="flex flex-wrap justify-center items-center gap-10 py-8">
         {data?.map((item, index) => {
           return (
@@ -31,12 +79,15 @@ const Information = () => {
               key={item.id}
             >
               <div className="flex flex-col p-8 rounded-xl bg-white shadow-xl translate-x-4 translate-y-4 w-72 sm:w-96 md:w-auto">
-                <div className="mt-3 font-semibold text-lg">{item.id}.</div>
-                <div className="text-sm font-normal w-60 md:w-auto">
+                <div className="mt-3 font-semibold text-2xl">{item.id}.</div>
+                {/* <div className="text-xl font-semibold w-60 md:w-auto">
                   {item.title}
-                </div>
+                </div> */}
                 <div className="my-4">
-                  <span className="font-light text-sm">{item.body}</span>
+                  <li>{item.list1}</li>
+                  <li>{item.list2}</li>
+                  <li>{item.list3}</li>
+                  <li>{item.list4}</li>
                 </div>
               </div>
             </div>
@@ -46,4 +97,4 @@ const Information = () => {
     </div>
   );
 };
-export default Information;
+export default LifeSkillsTraining;
